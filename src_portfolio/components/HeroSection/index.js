029389b5d15963a446949e8ff5653a156522038b
Matt Rebold <72838345+MattRebold@users.noreply.github.com>
2021-11-29 12:31:52 -0800

    Add files via upload

---
+++ src_portfolio/components/HeroSection/index.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import Video2 from "../../videos/video2.mp4";
+import { Button } from "../ButtonElement";
+import {
+  HeroContainer,
+  HeroBg,
+  VideoBg,
+  HeroContent,
+  HeroH1,
+  HeroP,
+  HeroBtnWrapper,
+  ArrowForward,
+  ArrowRight,
+} from "./HeroElements";
+
+const HeroSection = () => {
+  const [hover, setHover] = useState(false);
+
+  const onHover = () => {
+    setHover(!hover);
+  };
+
+  return (
+    <HeroContainer id="home">
+      <HeroBg>
+        <VideoBg autoPlay loop muted src={Video2} type="video/mp4" />
+      </HeroBg>
+      <HeroContent>
+        <HeroH1>Matt Rebold</HeroH1>
+        <HeroP>
+          I am a web developer with experience leveraging frameworks to provide
+          a robust synopsis for high-level overviews.
+        </HeroP>
+        <HeroBtnWrapper>
+          <Button
+            to="signup"
+            onMouseEnter={onHover}
+            onMouseLeave={onHover}
+            primary="true"
+            dark="true"
+            smooth={true}
+            duration={500}
+            spy={true}
+            exact="true"
+            offset={-80}
+          >
+            Get Started {hover ? <ArrowForward /> : <ArrowRight />}
+          </Button>
+        </HeroBtnWrapper>
+      </HeroContent>
+    </HeroContainer>
+  );
+};
+
+export default HeroSection;
---
+++ src_portfolio/pages/signin.js
@@ -0,0 +1,14 @@
+import React from "react";
+import SignIn from "../components/SignIn";
+import ScrollToTop from "../components/SignIn/ScrollToTop";
+
+const SigninPage = () => {
+  return (
+    <div>
+      <ScrollToTop />
+      <SignIn />
+    </div>
+  );
+};
+
+export default SigninPage;
